8c0b561c29326234a749fe4d5848b6d1e2dcc4f6
JuanJoZP <juanzpjose136@gmail.com>
2020-12-13 20:07:32 -0500

    comando set_role: crear y asignar rol

--- src/commands/role_register.js
+++ src/commands/role_register.js
@@ -1,3 +1,4 @@
+const { DiscordAPIError } = require("discord.js")
 const fs = require("fs")
 const path = require("path")
 const db = require("../../db/roles.json")
@@ -23,13 +24,15 @@ module.exports = {
     const channel = message.guild.channels.cache.get(role_channel)
 
     try {
-      channel.messages
-        .fetch(channel.lastMessageID)
-        .then((msg) => msg.react(args[1]))
+      channel.messages.fetch(channel.lastMessageID).then((msg) => msg.react(args[1]))
     } catch (e) {
       if (e instanceof TypeError) {
         message.reply(
           "El rol ha sido añadido a la base de datos, pero recuerde ejecutar el comando !setup para poder enviar el mensaje con la asignacion de roles"
+        )
+      } else if (e instanceof DiscordAPIError) {
+        message.reply(
+          `${args[1]} no es un emoji valido.\nEl uso correcto de este comando es: \`${this.name} ${this.usage}\``
         )
       }
     }
--- src/events/role.js
+++ src/events/role.js
@@ -1,19 +1,39 @@
-const registered_roles = require("../../db/roles.json")
+const db = require("../../db/roles.json")
 
 module.exports = {
   event: "messageReactionAdd",
   once: false,
-  run(messageReaction, user) {
+  async run(messageReaction, user) {
     const { role_channel } = require("../config.json")
 
     if (messageReaction.message.channel.id != role_channel) return
-    if (user.bot) return
 
     let is_registered = false
-    registered_roles.forEach((role) => {
+    db.forEach((role) => {
       if (role.emoji == messageReaction._emoji.name) is_registered = true
     })
 
     if (!is_registered) messageReaction.remove()
+
+    const db_role = db.find((role) => role.emoji == messageReaction._emoji.name)
+
+    if (messageReaction.message.guild.roles.cache.find((role) => role.name == db_role.role) == undefined) {
+      await messageReaction.message.guild.roles.create({
+        data: {
+          name: db_role.role,
+          hoist: false,
+          mentionable: true,
+        },
+        reason: "Geek Bot",
+      })
+    }
+
+    const role = messageReaction.message.guild.roles.cache.find((role) => role.name == db_role.role)
+
+    const member = messageReaction.message.guild.members.cache.get(user.id)
+
+    if (member.user.bot) return
+
+    member.roles.add(role)
   },
 }
